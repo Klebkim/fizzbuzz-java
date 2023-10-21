2c1bf6c79760874f5d311b5744e7fba1258edeec
Klebkim <132722047+Klebkim@users.noreply.github.com>
2023-10-21 10:18:56 +0900

    .m,kjbn jkmhnvbcmjnbvc

---
+++ src/FizzBuzz.java
@@ -0,0 +1,21 @@
+public class FizzBuzz {
+    public static void main(String[] args){
+        printFizzBuzz(10000);
+    }
+    public static void printFizzBuzz (int n){
+        for (int i = 1; i <= n; i++){
+            if (i % 15 == 0){
+                System.out.println(i+". FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println(i+". Fizz");
+            }
+                else if (i % 5 == 0) {
+                    System.out.println(i+". Buzz");
+                }
+                else{
+                    System.out.println(i + ".");
+            }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int myVar1 = 2;
+        int myVar2 = 3;
+        System.out.println(myVar1 + myVar2);
+
+        String myVar3 = "Ginny";
+        System.out.println(myVar3);
+
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+}
